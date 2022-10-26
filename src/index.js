9c2bbcb2c3046d99bf29ebbee0293b10929a6212
Daniel <daniel.hammami@yahoo.com>
2022-10-26 14:30:43 +0200

    currying

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let input = "   JavaScript   ";
 let output = "<div>" + input.trim() + "</div>";
 
 const trim = (str) => str.trim();
-const wrapInDiv = (str) => `<div>${str}</div>`;
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
+const result = wrap(toLowerCase(trim(input)));
