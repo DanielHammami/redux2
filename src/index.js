9fa9bb7076c341a28345890186eccada5652d8ec
Daniel <daniel.hammami@yahoo.com>
2022-10-26 14:51:35 +0200

    pure_functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-let input = "   JavaScript   ";
-let output = "<div>" + input.trim() + "</div>";
-
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-const result = wrap(toLowerCase(trim(input)));
+function isEligible(age, minAge) {
+  return age > minAge;
+}
