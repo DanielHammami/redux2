13e213095f2e1e184d4ab0f061b7749a3923ab3e
Daniel <daniel.hammami@yahoo.com>
2022-10-26 16:00:40 +0200

    Immutability_updating_object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-function isEligible(age, minAge) {
-  return age > minAge;
-}
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "San Francisco",
+  },
+};
+const updated = Object.assign({}, person, { name: "Bob", age: 30 });
+// => equal
+const updated = { ...person, name: "Bob" };
+
+updated.address.city = "New York";
+
+/*
+log person =>
+
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "New York",
+  },
+};
+*/
+
+const updated = {
+  ...person,
+  address: {
+    ...person.address,
+    city: "New York",
+  },
+  name: "Bob",
+};
+
+/*
+log person =>
+
+const person = {
+  name: "John",
+  address: {
+    country: "USA",
+    city: "San Francisco",
+  },
+};
+*/
